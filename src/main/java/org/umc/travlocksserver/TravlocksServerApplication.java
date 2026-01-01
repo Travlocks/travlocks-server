37f9bee6867de68067696e9b21e8430e2aee22e2
hyomee2 <s5763305@naver.com>
2026-01-02 00:36:16 +0900

    [init] 프로젝트 초기 세팅

---
+++ src/main/java/org/umc/travlocksserver/TravlocksServerApplication.java
@@ -0,0 +1,13 @@
+package org.umc.travlocksserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravlocksServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravlocksServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/umc/travlocksserver/TravlocksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umc.travlocksserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravlocksServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
