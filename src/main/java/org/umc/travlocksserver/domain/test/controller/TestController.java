f8a048b8a01baa5b71f2301a78dcd9658a048ad9
kdhdd <kdso10@naver.com>
2026-01-05 19:05:47 +0900

    [test] Exception 테스트 추가

--- src/main/java/org/umc/travlocksserver/domain/test/controller/TestController.java
+++ src/main/java/org/umc/travlocksserver/domain/test/controller/TestController.java
@@ -1,13 +1,43 @@
 package org.umc.travlocksserver.domain.test.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
+import java.util.Map;
 
-@RestController("/api")
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.umc.travlocksserver.domain.test.dto.TestRequest;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
 public class TestController {
 
 	@GetMapping
 	public String test() {
 		return "API documents";
+	}
+
+	@GetMapping("/header-test")
+	public String test(
+		@RequestHeader(name = "Custom-Header")
+		String customHeader) {
+		return "Header value is: " + customHeader;
+	}
+
+	@GetMapping(value = "/media-test", produces = "application/xml")
+	public Map<String, String> mediaTest() {
+		return Collections.singletonMap("result", "ok");
+	}
+
+	@PostMapping("/valid-test")
+	public String test(
+		@Valid @RequestBody
+		TestRequest request) {
+		return "success";
 	}
 }
