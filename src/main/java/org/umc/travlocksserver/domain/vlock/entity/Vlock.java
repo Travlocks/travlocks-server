982c40b9ae35f7d05b0bb87dfcfadaa0c11a6465
Dohee Kwon <doeesoioi3@gmail.com>
2026-01-15 16:09:33 +0900

    [feat] #16 Vlock 엔티티 매핑

---
+++ src/main/java/org/umc/travlocksserver/domain/vlock/entity/Vlock.java
@@ -0,0 +1,67 @@
+package org.umc.travlocksserver.domain.vlock.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.umc.travlocksserver.domain.city.entity.City;
+import org.umc.travlocksserver.domain.member.entity.Member;
+import org.umc.travlocksserver.domain.vlockcategory.entity.VlockCategory;
+import org.umc.travlocksserver.global.entity.SoftDeleteBaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Entity
+@Table(name = "vlocks")
+public class Vlock extends SoftDeleteBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vlock_id")
+    private Long id;
+
+    /** 블록 카테고리 */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vlock_category_id", nullable = false)
+    private VlockCategory vlockCategory;
+
+    /** 도시 */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    /** 소유자 (외부 API 블록은 NULL 가능) */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Member owner;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "cover_img_url", nullable = false, length = 255)
+    private String coverImgUrl;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longtitude;
+
+    @Column(nullable = false, length = 255)
+    private String address;
+
+    @Column(length = 200)
+    private String memo;
+
+    @Column(name = "link_url", length = 255)
+    private String linkUrl;
+
+    @Column(name = "avg_rating")
+    private Double avgRating;
+
+    @Column(name = "usage_count", nullable = false)
+    private Integer usageCount;
+
+    @Column(name = "is_public", nullable = false)
+    private Boolean isPublic;
+}
