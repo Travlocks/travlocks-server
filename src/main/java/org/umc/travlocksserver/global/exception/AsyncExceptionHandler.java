948ddc227cccfa73436a53036ad9b5de2e79d03b
kdhdd <kdso10@naver.com>
2026-01-29 15:51:42 +0900

    [feat] #23 비동기 예외 처리 추가

---
+++ src/main/java/org/umc/travlocksserver/global/exception/AsyncExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.umc.travlocksserver.global.exception;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+	@Override
+	public void handleUncaughtException(@NonNull Throwable ex, @NonNull Method method, @Nullable Object... params) {
+		log.error("[Async Error] Method: {}, Message: {}",
+			method.getName(), ex.getMessage(), ex);
+	}
+}
