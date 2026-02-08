e3f53ee3119021fe8f4d98af194ee48f913241dd
kdhdd <kdso10@naver.com>
2026-02-08 16:28:54 +0900

    [feat] #100 Add S3 Settings

---
+++ src/main/java/org/umc/travlocksserver/global/aws/S3Path.java
@@ -0,0 +1,6 @@
+package org.umc.travlocksserver.global.aws;
+
+public final class S3Path {
+
+	public static final String S3_DOMAIN = "https://travlocks-bucket.s3.ap-northeast-2.amazonaws.com/";
+}
---
+++ src/main/java/org/umc/travlocksserver/global/aws/S3Provider.java
@@ -0,0 +1,73 @@
+package org.umc.travlocksserver.global.aws;
+
+import static org.umc.travlocksserver.global.aws.S3Path.*;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+import org.umc.travlocksserver.global.code.ErrorCode;
+import org.umc.travlocksserver.global.exception.handler.S3ExceptionHandler;
+
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+@Component
+@RequiredArgsConstructor
+public class S3Provider {
+
+	private final S3Client s3Client;
+
+	@Value("${aws.s3.bucket}")
+	private String bucket;
+
+	public String uploadVlockFile(MultipartFile file) {
+		if (file == null || file.isEmpty()) {
+			return null;
+		}
+
+		String fileName = "vlocks/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+		try {
+			PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+				.bucket(bucket)
+				.key(fileName)
+				.contentType(file.getContentType())
+				.build();
+
+			s3Client.putObject(putObjectRequest,
+				RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+		} catch (IOException e) {
+			throw new S3ExceptionHandler(ErrorCode.FILE_READ_ERROR);
+		} catch (S3Exception e) {
+			throw new S3ExceptionHandler(ErrorCode.S3_UPLOAD_FAIL);
+		}
+
+		return String.format(S3_DOMAIN + "%s", fileName);
+	}
+
+	public void deleteFile(String fileUrl) {
+		if (fileUrl == null || !fileUrl.contains(bucket)) {
+			return;
+		}
+
+		String key = fileUrl.substring(fileUrl.lastIndexOf(".com/") + 5);
+
+		try {
+			DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+				.bucket(bucket)
+				.key(key)
+				.build();
+
+			s3Client.deleteObject(deleteObjectRequest);
+		} catch (S3Exception e) {
+			throw new S3ExceptionHandler(ErrorCode.S3_DELETE_FAIL);
+		}
+	}
+}
