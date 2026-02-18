6253020d8a8af059b60a9926b6fa9826da697889
dppfls <a93864140@gmail.com>
2026-02-19 06:34:57 +0900

    [refactor] #168 AWS Access Key 제거 및 IAM Role 기반 인증으로 전환

--- src/main/java/org/umc/travlocksserver/global/config/S3Config.java
+++ src/main/java/org/umc/travlocksserver/global/config/S3Config.java
@@ -12,22 +12,13 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-	@Value("${aws.credentials.access-key}")
-	private String accessKey;
-
-	@Value("${aws.credentials.secret-key}")
-	private String secretKey;
-
 	@Value("${aws.region}")
 	private String region;
 
 	@Bean
 	public S3Client s3Client() {
-		AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
 		return S3Client.builder()
 			.region(Region.of(region))
-			.credentialsProvider(StaticCredentialsProvider.create(credentials))
 			.build();
 	}
 }
