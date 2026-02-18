b51263ec6989f1079625cf7b437cf0b43e87fb2e
hyomee2 <s5763305@naver.com>
2026-02-18 20:28:02 +0900

    [refactor] #157 최적 동선 정렬 엔드포인트 GET -> POST로 변경

--- src/main/java/org/umc/travlocksserver/domain/template/controller/TemplateController.java
+++ src/main/java/org/umc/travlocksserver/domain/template/controller/TemplateController.java
@@ -6,15 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
 import org.umc.travlocksserver.domain.template.dto.request.TemplatePreInputRequestDTO;
 import org.umc.travlocksserver.domain.template.dto.response.TemplatePreInputResponseDTO;
 import org.umc.travlocksserver.domain.template.enums.TripDays;
@@ -46,7 +38,6 @@ import org.umc.travlocksserver.domain.template.dto.response.TemplateVlockReorder
 import org.umc.travlocksserver.domain.template.service.command.TemplateCommandService;
 import org.umc.travlocksserver.domain.template.dto.request.TemplateSaveRequestDTO;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 import org.umc.travlocksserver.domain.template.dto.response.TemplateSaveResponseDTO;
 import org.umc.travlocksserver.domain.template.service.query.TemplateSummaryQueryService;
@@ -240,7 +231,7 @@ public class TemplateController implements TemplateControllerDocs {
 			SuccessResponse.ok(TemplateDaySuccessCode.VLOCK_SUGGESTION_SUCCESS, response));
 	}
 
-	@GetMapping("/{templateId}/days/{dayNo}/optimize")
+	@PostMapping("/{templateId}/days/{dayNo}/optimize")
 	public ResponseEntity<SuccessResponse<OptimizeResponseDTO>> optimize(
 		@AuthenticationPrincipal
 		Long memberId,
